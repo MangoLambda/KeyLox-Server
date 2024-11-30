7740dda0f05bc6430788454c66bc850a3451b31c
MangoLambda <mangolambda@users.noreply.github.com>
2024-11-30 04:07:51 +0000

    Updated register handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -16,15 +16,16 @@ import (
 // @Tags user
 // @Produce json
 // @Param username path string true "Username"
-// @Success 200 {object} models.User
-// @Failure 500 {object} map[string]string
+// @Success 200 {object} models.UserResponse
+// @Success 204 {object} string
+// @Failure 500 {object} string
 // @Router /user/{username} [get]
 func GetUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		username := chi.URLParam(r, "username")
 
-		var user models.User
+		var user models.DBUser
 		rows, err := db.Query("SELECT id, username, clientSalt FROM users WHERE username = ?", username)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -37,9 +38,11 @@ func GetUser(db *sql.DB) http.HandlerFunc {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			json.NewEncoder(w).Encode(user)
+			var userResponse models.UserResponse
+			userResponse.Salt = user.ClientSalt
+			json.NewEncoder(w).Encode(userResponse)
 		} else {
-			http.Error(w, "User not found", http.StatusNotFound)
+			http.Error(w, "User not found", http.StatusNoContent)
 		}
 	}
 }
@@ -49,47 +52,53 @@ func GetUser(db *sql.DB) http.HandlerFunc {
 // @Tags users
 // @Accept json
 // @Produce json
-// @Param user body models.User true "User"
-// @Success 200 {object} models.User
-// @Failure 409 {object} map[string]string
-// @Failure 500 {object} map[string]string
+// @Param user body models.RegisterRequest true "User"
+// @Success 200 {object} string
+// @Failure 409 {object} string
+// @Failure 500 {object} string
 // @Router /register [post]
 func Register(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var user models.User
-		json.NewDecoder(r.Body).Decode(&user)
-		// Check if the username already exists and get clientSalt if it does
-		var clientSalt sql.NullString
-		err := db.QueryRow("SELECT clientSalt FROM users WHERE username = ?", user.Username).Scan(&clientSalt)
+		var req models.RegisterRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Check if the username already exists
+		var existingUser models.UserResponse
+		err := db.QueryRow("SELECT clientSalt FROM users WHERE username = ?", req.Username).Scan(&existingUser.Salt)
 		if err != nil && err != sql.ErrNoRows {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		if clientSalt.Valid {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(map[string]string{"clientSalt": clientSalt.String})
+
+		if err == nil {
+			// Username already exists
 			http.Error(w, "User already exists", http.StatusConflict)
 			return
 		}
 
 		// Set server salt, vault id, and last login
-		user.ServerSalt = "some-generated-server-salt" // Replace with actual salt generation logic
-		user.VaultId = 0                               // Replace with actual vault ID logic if needed
-		user.LastLogin = time.Now()
-		result, err := db.Exec("INSERT INTO users (username, key, clientSalt, serverSalt, vaultId, lastLogin) VALUES (?, ?, ?, ?, ?, ?)",
-			user.Username, user.Key, user.ClientSalt, user.ServerSalt, user.VaultId, user.LastLogin)
+		user := models.DBUser{
+			Username:   req.Username,
+			Key:        req.Key,
+			ClientSalt: req.ClientSalt,
+			ServerSalt: "some-generated-server-salt", // Replace with actual salt generation logic
+			HashedKey:  "some-hashed-key",            // Replace with actual key hashing logic
+			VaultId:    1,                            // Replace with actual vault ID logic if needed
+			LastLogin:  time.Now(),
+		}
+
+		_, err = db.Exec("INSERT INTO users (username, key, clientSalt, serverSalt, hashedKey, vaultId, lastLogin) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			user.Username, user.Key, user.ClientSalt, user.ServerSalt, user.HashedKey, user.VaultId, user.LastLogin)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		id, err := result.LastInsertId()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		user.ID = int(id)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(user)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode("User registered successfully")
 	}
 }
 
@@ -98,10 +107,10 @@ func Register(db *sql.DB) http.HandlerFunc {
 // @Tags vaults
 // @Produce json
 // @Param id path int true "Vault ID"
-// @Success 200 {object} models.Vault
-// @Failure 400 {object} map[string]string
-// @Failure 404 {object} map[string]string
-// @Failure 500 {object} map[string]string
+// @Success 200 {object} models.VaultResponse
+// @Failure 400 {object} string
+// @Failure 404 {object} string
+// @Failure 500 {object} string
 // @Router /vault/{id} [get]
 func GetVault(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -113,7 +122,7 @@ func GetVault(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		var vault models.Vault
+		var vault models.DBVault
 		err = db.QueryRow("SELECT id, userId, filename, created_at FROM vaults WHERE id = ?", id).Scan(&vault.ID, &vault.UserId, &vault.FileName, &vault.CreatedAt)
 		if err == sql.ErrNoRows {
 			http.Error(w, "Vault not found", http.StatusNotFound)
