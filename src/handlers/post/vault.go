15590e0dcfaacfba6243b1302b06105f18e90e8a
MangoLambda <mangolambda@users.noreply.github.com>
2024-12-01 00:24:08 +0000

    initial file creation

--- src/handlers/post/vault.go
+++ src/handlers/post/vault.go
@@ -2,11 +2,16 @@ package handlers
 
 import (
 	"database/sql"
-	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
-	"github.com/MangoLambda/KeyLox-Server/src/models"
+	"golang.org/x/crypto/sha3"
 )
+
+const maxUploadSize = 2 << 20  // 2 MB
+const allocationSize = 3 << 20 // 3 MB
 
 // TODO: Fix docs
 // @Summary Upload a vault
@@ -21,38 +26,55 @@ import (
 // @Router /register [post]
 func VaultHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var registerRequest models.RegisterRequest
-		if err := json.NewDecoder(r.Body).Decode(&registerRequest); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if err := validateInputs(registerRequest); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Check if the username already exists
-		var exists, err = checkUsernameExists(db, registerRequest.Username)
+		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+		err := r.ParseMultipartForm(allocationSize)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Unable to parse form", http.StatusBadRequest)
 			return
 		}
 
-		if exists {
-			// Username already exists
-			http.Error(w, "User already exists", http.StatusConflict)
-			return
-		}
-
-		// Register the user
-		err = registerUser(db, registerRequest)
+		// Retrieve the file from form data
+		file, handler, err := r.FormFile("file")
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		if handler.Size > maxUploadSize {
+			http.Error(w, "The file is too big. 2 MB max.", http.StatusBadRequest)
 			return
 		}
 
+		// Compute the SHA3-512 hash of the file
+		hasher := sha3.New512()
+		if _, err := io.Copy(hasher, file); err != nil {
+			http.Error(w, "Unable to compute hash", http.StatusInternalServerError)
+			return
+		}
+		hashSum := hasher.Sum(nil)
+		hashFilename := fmt.Sprintf("%x", hashSum)
+
+		// Reset the file pointer to the beginning
+		file.Seek(0, io.SeekStart)
+
+		// Create a file on disk
+		dst, err := os.Create("/vaults/" + hashFilename)
+		if err != nil {
+			http.Error(w, "Unable to create the file on disk", http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		// Copy the uploaded file to the created file on disk
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			http.Error(w, "Unable to save the file", http.StatusInternalServerError)
+			return
+		}
+
+		// Respond to the client
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("User registered successfully")
+		w.Write([]byte("File uploaded successfully"))
 	}
 }
